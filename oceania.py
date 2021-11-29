724b4c3367871d7d40823c3111d39ecc6442cb07
Lamprini Papargyri <80075276+papargyri@users.noreply.github.com>
2021-11-29 16:45:30 +0200

    Create oceania.py

---
+++ oceania.py
@@ -0,0 +1,16 @@
+countries_oceania = [
+ 'Australia',
+ 'Fiji',
+ 'Kiribati',
+ 'Marshall Islands',
+ 'Micronesia',
+ 'Nauru',
+ 'New Zealand',
+ 'Palau',
+ 'Papua New Guinea',
+ 'Samoa',
+ 'Solomon Islands',
+ 'Tonga',
+ 'Tuvalu',
+ 'Vanuatu'
+]
